8d6694022366ff3854ec24f2913ac755fd4cb5b5
Julien <ju.pellegrin16@gmail.com>
2019-04-21 14:41:23 +0200

    Modification du fichier test.py

--- test.py
+++ test.py
@@ -3,9 +3,9 @@
 #print(multiplication(2,10))
 
 import os
-#import package.firstProgramme
-#print(package.firstProgramme.multiplication(3,10))
-#os.system("Pause")
+import package.firstProgramme
+print(package.firstProgramme.multiplication(3,10))
+os.system("Pause")
 
-from package.casino import *
-roulette()
+#from package.casino import *
+#roulette()
